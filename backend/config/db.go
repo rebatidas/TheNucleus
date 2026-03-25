949d5764078e27af8515eff1572e3ca75f271d41
Ruhi Reddy <111948952+ruhireddy@users.noreply.github.com>
2026-03-25 15:57:33 -0400

    Update db.go

--- backend/config/db.go
+++ backend/config/db.go
@@ -12,8 +12,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=Revtee@9876 dbname=TheNucleus port=5432 sslmode=disable"
-
+	dsn := "host=localhost user=postgres password=pass dbname=thenucleus port=5433 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
